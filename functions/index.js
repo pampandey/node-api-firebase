dbb4e624de0b78b4260a684b7f6052bc1d95cd71
Pramila Pandey <pam.pandey@Pramilas-MacBook-Air.local>
2022-07-28 15:48:40 -0400

    pushed to firebase cloud functions

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+// setup routes...
+app.get('/test', (req,res) => {
+    res.send('Wow, it actually worked.')
+});
+
+app.get('/another-one', (req,res) => {
+    res.send('Wow,it actually worked!')
+});
+
+
+//app.get('/helloThere',)
+
+exports.api = functions.https.onRequest(app);
+
+exports.helloThere = functions.https.onRequest((req,res) => {
+    res.send('Hello there Firebase Cloud user!');
+});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
